035f5e64e60094c612fbfba58fe5806918efeac3
Narek Yazeryan <yazeryann@gmail.com>
2023-04-25 01:51:36 +0400

    fix(algo-binary-search) Bugfixing

--- src/algorithms/search/binarySearchIndex.ts
+++ src/algorithms/search/binarySearchIndex.ts
@@ -1,4 +1,4 @@
-const binarySearchIndex = <T>(
+const binarySearchIndex = <T = any>(
   array: T[],
   search: T,
   prevStart = 0,
@@ -12,6 +12,9 @@ const binarySearchIndex = <T>(
   const pivot = array[pivotIndex];
   if (pivot === search) return pivotIndex;
 
+  // Not Found Case
+  if (prevEnd - prevStart <= 1) return -1;
+
   const start = pivot > search ? prevStart : pivotIndex;
   const end = pivot > search ? pivotIndex : prevEnd;
 
@@ -20,7 +23,7 @@ const binarySearchIndex = <T>(
 
 const getPivot = (start: number, end: number) => {
   const length = end - start;
-  if (length <= 1) return 1;
+  if (length <= 1) return 0;
   const pivot =  Math.ceil(length / 2);
 
   return start + pivot;
